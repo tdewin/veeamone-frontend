667cebd8243dfd77d72f2708e49aa4ba2039ca3c
tdewin <tdewin@users.noreply.github.com>
2021-04-09 11:07:23 +0200

    adv + sectioning
    Advanced paramters are now filled with the result of a simple call so that they are relevant for the calculation and that the amount of parameters can be reduced (no more "Ratio" parameters)
    
    Sectioning is enforced by hiding the result initially

--- static/js/gui.js
+++ static/js/gui.js
@@ -1,6 +1,6 @@
-window.resturl = window.location.protocol+"//localhost:4001"
+window.resturl = "http://localhost"
 
-//if served over https, fetch over https
+
 
 
 
@@ -19,7 +19,6 @@ function scrollDiv(divid) {
 
 $("#idAdvanced").click(()=> {
     if (advancedEnabled()) {
-        $("#idDivAdvanced").show()
         $("#idBtnNext1").val("Advanced")
     } else {
         $("#idDivAdvanced").hide()
@@ -34,23 +33,17 @@ $("#idAdvanced").click(()=> {
 window.advancedParameters = new Vue({
     el: '#advancedParameters',
     data: { "advancedFields": [
-        {"id":"datastoreRatio","valfield": 200,"label":"Datastore Ratio"},
-        {"id":"rPoolRatio","valfield": 100,"label":"Resource Pool Ratio"},
-        {"id":"clusterRatio","valfield": 10,"label":"Cluster Ratio"},
-        {"id":"vappRatio","valfield": 50,"label":"vApp Ratio"},
-        {"id":"avNumDsOneVm","valfield": 0,"label":"Avg Number Of Datastore Per VM"},
-        {"id":"avNicsHost","valfield": 4,"label":"Avg NICs Per Host"},
-        {"id":"avNicsVM","valfield": 1,"label":"Avg NICs Per VM"},
-        {"id":"avNumVDisksVm","valfield": 3,"label":"Avg Num Virtual Disks Per VM"},
-        {"id":"avNumGDiskVm","valfield": 0,"label":"Avg Num Global Disks Per VM"},
-        {"id":"avSdPerHost","valfield": 2,"label":"Avg Storage Adapter Per Host"},
-        {"id":"avSpPerHost","valfield": 2,"label":"Avg Storage Path Per Host"},
-        {"id":"eventsHistory","valfield": 12,"label":"Event History Data in Months"},
-        {"id":"vmThreashold","valfield": 1500,"label":"VM Threshold Adv Scaling"},
-        {"id":"resourcePoolQty","valfield": 2,"label":"Resource Pools"},
-        {"id":"clusterQty","valfield": 1,"label":"Clusters"},
-        {"id":"vappQty","valfield": 4,"label":"vApps"},
-        {"id":"datastoreQty","valfield": 1,"label":"Datastores"}
+        {"id":"vmThreshold","valfield": 1500,"label":"VM Threshold Adv Scaling","info":"Starting from this threshold, advanced scaling is used"},
+        {"id":"resourcePoolQty","valfield": 8,"label":"Resource Pools","info":"Total amount of Resource Pools in your environment"},
+        {"id":"clusterQty","valfield": 1,"label":"Clusters","info":"Total amount of Clusters in your environment"},
+        {"id":"vappQty","valfield": 4,"label":"vApps","info":"Total amount of vApps in your environment"},
+        {"id":"datastoreQty","valfield": 1,"label":"Datastores","info":"Total amount of Datastores in your environment"},
+        {"id":"avNicsVM","valfield": 1,"label":"Avg NICs Per VM","info":"Average amount of NICs per Virtual Machine"},
+        {"id":"avNumVDisksVm","valfield": 3,"label":"Avg Num Virtual Disks Per VM","info":"Average amount of Virtual Disks (VMDK/VHD) per Virtual Machine"},
+        {"id":"avNicsHost","valfield": 4,"label":"Avg NICs Per Host","info":"Average amount of NICs per host (ESXi/HyperV)"},
+        {"id":"avSdPerHost","valfield": 2,"label":"Avg HBA Per Host","info":"Average amount of Storage Adapater per host (ESXi/HyperV)"},
+        {"id":"avSpPerHost","valfield": 2,"label":"Avg Storage Path Per Host","info":"Average amount of Paths per host (ESXi/HyperV)"},
+        {"id":"eventsHistory","valfield": 12,"label":"Event History Months","info":"Event History override per Month. By default History performance data setting is copied"},
     ] },
   })
 
@@ -85,7 +78,7 @@ window.dbresult = new Vue({
                 "data": {
                     "sub": [],
                     "total": {
-                      "id":"totalCap","label":"Database Size (1.2x)","val":0,
+                      "id":"totalCap","label":"Database Size (1.2x) GB","val":0,
                     }
                 }
             }
@@ -110,8 +103,44 @@ function downloadDB() {
     });
     $('#dbdownload').prop("href","data:text/plain,"+encodeURI(ln.join("\n")))
 }
+
+function getDefaults() {
+    var calcurl = window.resturl+"/simpleCal" 
+    var dataIn = {
+        "vmQty": parseInt($("#idVM").val()),
+        "hostQty": parseInt($("#idHost").val()),
+        "historyMonths": parseInt($("#idHistory").val())
+    }
+
+    fetch(calcurl, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(dataIn) // body data type must match "Content-Type" header
+    }).then(response => response.json()).then(data => {
+        window.advancedParameters.advancedFields.forEach(field => {
+            if (field.id in data.settings) {
+                field.valfield = data.settings[field.id]
+            }
+        })
+       
+        $("#idDivAdvanced").show()   
+        scrollDiv("#idDivAdvanced")
+    }).catch((error) => {
+        console.error('Error:', error);
+        $('#modalerrortext').html("<p>Issue while fetching result from the server, please retry.</p><p>If the issue persists, the backend might be experiencing issues</p><p>"+error+"</p>")
+    }).finally(()=> {
+        $("#idBtnNext1").prop('disabled', false);
+        $("#idBtnNext2").prop('disabled', false);
+    })
+}
 function recalculateResult(advanced=false) {
     console.log("recalculating")
+
+    $("#idDivResult").hide()
 
     var calcurl = window.resturl+"/simpleCal"
     var dataIn = {
@@ -126,9 +155,7 @@ function recalculateResult(advanced=false) {
         });
         dataIn["settings"] = advparams
         calcurl = window.resturl+"/advancedCal"
-    } else {
-        
-    }
+    } 
 
     const u = "Updating ..."
     window.dbresult.sections.forEach(section => {
@@ -187,15 +214,15 @@ function recalculateResult(advanced=false) {
         $("#idBtnNext2").prop('disabled', false);
     })
 
-
+    $("#idDivResult").show()
     
 }
 
 
 $("#idBtnNext1").click(function() {
     if (advancedEnabled()) {
-        $("#eventsHistory").val(parseInt($("#idHistory").val()))
-        scrollDiv("#idDivAdvanced")
+        //$("#eventsHistory").val(parseInt($("#idHistory").val()))
+        getDefaults()
     } else {
         //if not advanced, calculate the result
         recalculateResult(false)
@@ -222,5 +249,9 @@ $( document ).ready(function(){
         $("#idAdvanced").prop( "checked", true );
         $("#idDivAdvanced").show()
     }
+
+   
+    $('[data-toggle="tooltip"]').tooltip()
+      
 })
 
